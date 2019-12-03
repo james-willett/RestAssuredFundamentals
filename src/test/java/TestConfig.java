a9ee48d09c7724a8ed0904faa03b507280f83742
James Willett <james.willett@sap.com>
2019-12-03 11:30:23 +0000

    Adding code for proxy

--- src/test/java/TestConfig.java
+++ src/test/java/TestConfig.java
@@ -5,6 +5,9 @@ public class TestConfig {
 
     @BeforeClass
     public static void setup() {
+
+        RestAssured.proxy("localhost", 8888);
+
         RestAssured.baseURI = "http://localhost";
         RestAssured.basePath = "/app/";
         RestAssured.port = 8080;
