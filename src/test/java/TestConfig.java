1ddac45bf245ed3aa32d2972fbf52f702ae047ec
James Willett <james.willett@sap.com>
2019-12-03 11:49:18 +0000

    Adding code for request spec

--- src/test/java/TestConfig.java
+++ src/test/java/TestConfig.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.BeforeClass;
 
 public class TestConfig {
@@ -6,10 +8,17 @@ public class TestConfig {
     @BeforeClass
     public static void setup() {
 
-        RestAssured.proxy("localhost", 8888);
+     //   RestAssured.proxy("localhost", 8888);
 
         RestAssured.baseURI = "http://localhost";
         RestAssured.basePath = "/app/";
         RestAssured.port = 8080;
+
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Accept", "application/json")
+                .build();
+
+        RestAssured.requestSpecification = requestSpecification;
     }
 }
