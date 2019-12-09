d82ba2650c7598815bb44a82810aa715c7175c39
James Willett <james.willett@sap.com>
2019-12-09 22:35:06 +0000

    Adding code for assert body

--- src/test/java/FootballApiTests.java
+++ src/test/java/FootballApiTests.java
@@ -1,6 +1,13 @@
 import config.FootballApiConfig;
+import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class FootballApiTests extends FootballApiConfig {
 
@@ -11,6 +18,25 @@ public class FootballApiTests extends FootballApiConfig {
                 .queryParam("areas", 2255).
         when()
             .get("areas");
+    }
+
+    @Test
+    public void getDateFounded() {
+        given().
+        when()
+                .get("teams/57").
+        then()
+                .body("founded", equalTo(1886));
 
     }
+
+    @Test
+    public void getFirstTeamName() {
+        given().
+        when()
+                .get("competitions/2021/teams").
+        then().
+                body("teams.name[0]", equalTo("Arsenal FC"));
+    }
+    
 }
