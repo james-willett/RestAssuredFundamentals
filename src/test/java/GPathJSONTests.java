47a19ae760b87d7ba76605c9703bef42aaaa74f2
James Willett <james.willett@sap.com>
2019-12-12 09:01:30 +0000

    Adding code for gpath json find all

--- src/test/java/GPathJSONTests.java
+++ src/test/java/GPathJSONTests.java
@@ -2,6 +2,7 @@ import config.FootballApiConfig;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -17,6 +18,19 @@ public class GPathJSONTests extends FootballApiConfig {
                 ("teams.find { it.name == 'Manchester United FC' }");
 
         System.out.println("Map of Team Data = " + allTeamDataForSingleTeam);
+    }
 
+    @Test
+    public void extractSingleValueWithFind() {
+        Response response = get("teams/57");
+        String certainPlayer = response.path("squad.find { it.shirtNumber == 23}.name");
+        System.out.println("Name of player = " + certainPlayer);
+    }
+
+    @Test
+    public void extractListOfValuesWithFindAll() {
+        Response response = get("teams/57");
+        List<String> playerNames = response.path("squad.findAll { it.shirtNumber >= 23}.name");
+        System.out.println("List of players" + playerNames);
     }
 }
