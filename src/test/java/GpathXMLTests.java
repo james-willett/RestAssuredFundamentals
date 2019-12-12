cec09982fc3a5969334f660373f2f4cc53c35ef9
James Willett <james.willett@sap.com>
2019-12-12 13:59:35 +0000

    Adding code for Gpath Extract Nodes

--- src/test/java/GpathXMLTests.java
+++ src/test/java/GpathXMLTests.java
@@ -1,7 +1,11 @@
 import config.VideoGameConfig;
 import config.VideoGamesEndpoints;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.element.Node;
 import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -25,5 +29,17 @@ public class GpathXMLTests extends VideoGameConfig {
         String category = response.path("videoGames.videoGame[0].@category");
 
         System.out.println(category);
+    }
+
+    @Test
+    public void getListOfXmlNodes() {
+
+        String responseAsString = get(VideoGamesEndpoints.ALL_VIDEO_GAMES).asString();
+
+        List<Node> allResults = XmlPath.from(responseAsString).get(
+                "videoGames.videoGame.findAll { element -> return element }"
+        );
+
+        System.out.println(allResults.get(2).get("name").toString());
     }
 }
