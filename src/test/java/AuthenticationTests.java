a981a992b1d3ac4cf0f2f88d2db2c502b0d41555
James Willett <james.willett@sap.com>
2019-12-12 14:23:52 +0000

    Adding code for basic auth

--- src/test/java/AuthenticationTests.java
+++ src/test/java/AuthenticationTests.java
@@ -1,10 +1,29 @@
 import io.restassured.RestAssured;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class AuthenticationTests {
 
     @BeforeClass
     public static void setup() {
-      //  RestAssured.proxy("localhost", 8888); // only need this if you are running through a proxy
+        RestAssured.proxy("localhost", 8888); // only need this if you are running through a proxy
+    }
+
+    @Test
+    public void basicPreemptiveAuthTest() {
+        given().
+                auth().preemptive().basic("username", "password").
+        when().
+                get("http://localhost:8080/someEndpoint");
+    }
+
+    @Test
+    public void basicChallengedAuthTest() {
+        given().
+                auth().basic("username", "password").
+        when().
+                get("http://localhost:8080/someEndpoint");
     }
 }
